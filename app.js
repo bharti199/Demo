133a57552fb965fcafdaf7ec975f2f0f783bc2c5
Bharti Rani <bharti21rani@gmail.com>
2024-08-23 17:36:23 +0530

    Add code

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // Add New Features
+function getFee(isMember) {
+    return isMember ? '$2.00' : '$10.00';
+}
+let vr = "abc"
+console.log(vr ? '$2.00' : (isMember == "abc" ? '$2.00' : '$10.00'));
+
+console.log(getFee("no"));
+console.log(getFee(null));
+
+// let arg = false;
+
+// if(arg) {
+//     console.log('condition true');
+// }
+// else {
+//     console.log('condition false');
+
+// }
+// if(arg == false) {
+//     console.log('condition true');
+// }
+
+// ==
+// !=
+// !==
+// ===
+// >
+// <
+// <=
+// =>
